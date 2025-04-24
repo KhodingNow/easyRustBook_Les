abe6d4002a36ca773a0a821ee5901f6aa619f726
Melezwa26 <phalosenzwa@gmail.com>
2025-04-24 12:48:35 +0200

    RustFundamentalsEmphasis

--- src/main.rs
+++ src/main.rs
@@ -91,14 +91,135 @@ println!("Okay then {}", my_string);
 
 // Make print_countri
 
-    fn print_countri(igama_lelizwe: String) -> String {
+    fn print_countri(igama_lelizwe: &String) {
         println!("{}", igama_lelizwe);
-        igama_lelizwe // return it here
+        
     }
 
     let ilizwe = String::from("Zanzibar");
-    let ilizwe = print_countri(ilizwe); // we have to use let here to get the String back
-    print_countri(ilizwe); 
+    print_countri(&ilizwe); // we print Zanzibar
+    print_countri(&ilizwe); // Again...
+
+
+// Doing something similar with a mutable reference
+
+    fn add_hungary(counti_name: &mut String) { // first, we say the function takes a mutable reference
+        counti_name.push_str("-Hungary"); // push_str() adds a &str to a String
+        println!("Now it says: {}", counti_name);
+    }
+
+// fn main() {
+        let mut kanti = String::from("Qokolweni");
+        add_hungary(&mut kanti); // we also need to give it a mutable reference.
+
+//  }
+
 //}
+
+// Give references to functions
+
+     fn print_country(country_name: String) -> String {
+        println!("{}", country_name);
+        country_name
+}
+
+//fn main() {
+    let country = String::from("Wakanda");
+    let country = print_country(country);
+    print_country(country);
+
+//}
+
+// DOING the SAME thing better with REFERENCES (Borrowing)
+
+    fn print_khhantri(khantri_name: &String) {
+        println!("{}", khantri_name);
+}
+
+    //fn main() {
+        let ikhantri = String::from("Mbizana");
+        print_khhantri(&ikhantri);
+        print_khhantri(&ikhantri);
+//}
+
+// COPY types - resides in the stack n and small - copy types incl (intn floats, booleans, and char)
+    fn prints_a_number(number: i32) {
+        println!("{}", number);
+    }
+
+    //fn main() {
+        let my_num = 9;
+        prints_a_number(my_num);
+        prints_a_number(my_num);
+    //}
+
+// Using CLONE  to send it to the function, so the 'cntry' variable is still alive
+
+    fn prints_cntry(cntry_name: String) {
+        println!("{}", cntry_name);   
+
+    }
+
+    //fn main() {
+    let cntry = String::from("Kwazakhele");
+    prints_cntry(cntry.clone());
+    prints_cntry(cntry);
+
+    //}
+
+// CLONE takes a lot of memory(if its a book, one String can be a book
+// // CLONE then returns a book everytime its called) 
+
+//     fn get_length(input: String, iteration: usize) { // owns the String
+//         println!("Iteration {}: It's {} words long.", iteration, input.split_whitespace().count()); // splits to counts number of words
+
+//     }
+
+//     //fn main() {
+//         let mut my_sstring = String::new();
+//         for i in 0..10 {
+//             my_sstring.push_str("Here are some more words"); // push the words on
+//             get_length(my_sstring.clone(), i); // gives it a clone everytime
+//     // }
+//     } 
+
+// a better VERSION of the above code with a '&' reference
+
+    fn get_length(input: String) {
+        println!("It's {} words long", input.split_whitespace().count());
+    }
+
+    //fn main() {
+        let mut my_thing = String::new();
+        for _ in 0..15 {
+            my_thing.push_str("Nantso ke inkathazo");
+            get_length(my_thing.clone());
+    //    }
+    }
+
+// Variables WITHOUT values
+
+    fn loop_then_return(mut counter: i32) -> i32 {
+        loop {
+            counter += 1;
+            if counter % 50 == 0 {
+                break;
+            }
+        }
+        counter
+    }
+
+    //fn main() {
+        let my_nombolo;
+
+        {
+            my_nombolo = 100;           
+            
+        }
+        println!("{}", my_nombolo)
+    //}
+
+
+
 
 }
